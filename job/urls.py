aa780ba0065a363b2c056909b002cd478a4109d4
unknown <rafilouliki98@gmail.com>
2022-02-28 22:40:52 +0200

    Add Function Based Views Api

--- job/urls.py
+++ job/urls.py
@@ -12,14 +12,22 @@ urlpatterns = [
     path('',views.job_list,name="job_list"),
     path('add',views.add_job,name="add_job"),
     path('<str:slug>',views.job_detail,name="job_detail"),
-
+    #Generic Class Based Views Api
     path('api/jobs',viewsapi.Job_ListApi.as_view(),name='job_list_api'),
     path('api/jobs/<int:id>',viewsapi.Job_DetailApi.as_view(),name='job_detail_api'),
     path('api/category',viewsapi.Category_ListApi.as_view(),name='category_list_api'),
     path('api/category/<int:id>',viewsapi.Category_DetailApi.as_view(),name='category_detail_api'),
-
     path('api/apply',viewsapi.Apply_ListApi.as_view(),name='apply_list_api'),
     path('api/apply/<int:id>',viewsapi.Apply_DetailApi.as_view(),name='apply_detail_api'),
+    #End of Generic Class Based Views Api
 
+    #Function Based Views Api
+    path('api/jobsfunc',viewsapi.job_list_func_api , name='job_list_func_api'),
+    path('api/jobsfunc/<int:id>',viewsapi.job_detail_func_api , name='job_detail_func_api'),
+    path('api/categoryfunc',viewsapi.Category_list_func_api , name='Category_list_func_api'),
+    path('api/categoryfunc/<int:id>',viewsapi.Category_detail_func_api , name='Category_detail_func_api'),
+    path('api/applyfunc',viewsapi.Apply_list_func_api , name='Apply_list_func_api'),
+    path('api/applyfunc/<int:id>',viewsapi.Apply_detail_func_api , name='Apply_detail_func_api'),
+    #End of Function Based Views Api
     
 ]
--- job/viewsapi.py
+++ job/viewsapi.py
@@ -2,6 +2,8 @@ from .models import Job,apply,Category
 from .serializers import JobSerializer,ApplySerializer,CategorySerializer
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.decorators import api_view
+
 # Create your views here.
 
 #Generic Based Views
@@ -36,3 +38,51 @@ class Apply_DetailApi(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ApplySerializer
     queryset = apply.objects.all()
     lookup_field = 'id'
+
+#End Of Generic Class Based Views
+
+
+
+#Function Based Views
+@api_view(['GET'])
+def job_list_func_api(request):
+    all_jobs = Job.objects.all()
+    data = JobSerializer(all_jobs, many=True).data
+    return Response({'data':data})
+
+
+@api_view(['GET'])
+def job_detail_func_api(request,id):
+    job_detail = Job.objects.get(id=id)
+    data = JobSerializer(job_detail).data
+    return Response({'data':data})
+
+
+@api_view(['GET'])
+def Category_list_func_api(request):
+    all_Category = Category.objects.all()
+    data = CategorySerializer(all_Category, many=True).data
+    return Response({'data':data})
+
+
+
+@api_view(['GET'])
+def Category_detail_func_api(request,id):
+    Category_detail = Category.objects.get(id=id)
+    data = CategorySerializer(Category_detail).data
+    return Response({'data':data})
+
+
+@api_view(['GET'])
+def Apply_list_func_api(request):
+    all_Apply = apply.objects.all()
+    data = ApplySerializer(all_Apply, many=True).data
+    return Response({'data':data})
+
+
+
+@api_view(['GET'])
+def Apply_detail_func_api(request,id):
+    Apply_detail = apply.objects.get(id=id)
+    data = ApplySerializer(Apply_detail).data
+    return Response({'data':data})
